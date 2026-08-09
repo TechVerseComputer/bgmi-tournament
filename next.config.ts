8a12e1e7aec28f3d45a52618c71539c7f264d99e
TechVerseComputer <www.techverse@gmail.com>
2026-08-09 15:42:59 +0530

    Configure PWA settings in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
+import type { NextConfig } from "next";
 import withPWAInit from "@ducanh2912/next-pwa";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+};
 
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
-export default withPWA({});
+export default withPWA(nextConfig);
